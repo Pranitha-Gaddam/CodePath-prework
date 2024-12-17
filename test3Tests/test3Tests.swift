97a28a2a555e9d13b0cecbd7e3988d096b7aa0cb
Pranitha-Gaddam <gaddam.pranitha29@gmail.com>
2024-12-16 18:55:35 -0600

    Initial Commit

---
+++ test3Tests/test3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  test3Tests.swift
+//  test3Tests
+//
+//  Created by Pranitha on 12/16/24.
+//
+
+import Testing
+@testable import test3
+
+struct test3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
